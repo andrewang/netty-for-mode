eb963375e329c7f946c7982c9ddae06beb76cfbf
WXH <972612778@qq.com>
2016-09-26 16:15:32 +0800

    修改传输数据中的前4个字节为数据的长度

--- src/main/java/com/aries/network/codec/ByteBufCodecUtil.java
+++ src/main/java/com/aries/network/codec/ByteBufCodecUtil.java
@@ -23,12 +23,14 @@ public class ByteBufCodecUtil {
 	 */
 	public static CodeData codeDataDecode(ByteBuf buf){
 		if(buf.isReadable()){
+			int length =buf.readInt();
     		int code =buf.readInt();
     		byte[] data =ByteBufUtil.getBytes(buf, buf.readerIndex(), buf.readableBytes());
     		if(data.length<=maxDataLength-CommonType.INT_BYTES_SIZE){
     			CodeData codecData = new CodeData(code, data);
     			return codecData;
     		}
+    		System.out.println("data length: "+length);
     	}
 		return null;
 	}
@@ -38,11 +40,12 @@ public class ByteBufCodecUtil {
 	 * @return ByteBuf
 	 */
 	public static ByteBuf codeDataEncode(CodeData codeData){
-		int bytesLength=CommonType.INT_BYTES_SIZE + codeData.getData().length;
+		int bytesLength=CommonType.INT_BYTES_SIZE*2 + codeData.getData().length;
 		if(bytesLength>maxDataLength){
 			return null;
 		}
 		ByteBuf in = Unpooled.buffer(bytesLength);
+		in.writeInt(bytesLength);
 		in.writeInt(codeData.getCommandID());
 		in.writeBytes(codeData.getData());
 		return in;
